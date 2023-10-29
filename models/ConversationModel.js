8f04ffa325e53ebd5d7847f35873301b6326d8a2
Durundur <0bogusz0@gmail.com>
2023-10-29 19:52:48 +0100

    fixed chat

--- models/ConversationModel.js
+++ models/ConversationModel.js
@@ -14,7 +14,17 @@ const ConversationSchema = new mongoose.Schema({
         ref: 'AdsData',
     },
     lastMessage: {
-        type: String
+        message: String,
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'UsersData'
+        },
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+        },
+        timestamp: {
+            type: Date
+        }
     }
 }, {toJSON: {virtuals: true}, id: false})
 
